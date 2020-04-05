9cb3c9baa413161be9750c2e9d1d330c930a256d
Charles Butler <cdbutle@outlook.com>
2020-04-05 22:52:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,13 +13,14 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance > @amount && @status == "pending"
+    if @sender.balance < @amount && @status == "pending"
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    else
+      @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    else
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
     end
   end
 
