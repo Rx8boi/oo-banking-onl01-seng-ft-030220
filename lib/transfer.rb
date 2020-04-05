2dc858be971cfe264cd1a39a68fff5ec9a8920b6
Charles Butler <cdbutle@outlook.com>
2020-04-05 23:02:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
 
 
   def execute_transaction
-    if 
+    if
       @sender.balance >= @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
